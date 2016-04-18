04ac2bebe8a7fee595183a7160e2142626e9ca50
BinHong Lee <blee43@asu.edu>
2016-04-18 13:41:00 -0700

    Person
    Added Person class and header file.

---
+++ Person.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "Person.h"
+
+Person::Person(string name, string password, string email, string phoneNo)
+  {
+    this->name = name;
+    this->password = password;
+    this->email = email;
+    this->phoneNo = phoneNo;
+  }
---
+++ Person.h
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Person
+{
+  string name;
+  string password;
+  string email;
+  string phoneNo;
+
+  public :
+  Person(string, string, string, string);
+};
