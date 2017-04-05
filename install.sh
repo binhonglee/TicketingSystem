08191e2766613ae3a3dc6e85aa6962729890081a
BinHong Lee <binhong@binhong.me>
2017-04-05 11:02:53 -0700

    Changed command line argument to support older g++ versions

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-sudo apt-get install libjsonrpccpp-dev libjsonrpccpp-tools
+sudo apt-get install libjsonrpccpp-dev libjsonrpccpp-tools libjsoncpp-dev
+brew install libjson-rpc-cpp
 cp lib/anttasks.jar ~
 pushd ~
 jar xf anttasks.jar
